39c9bc6778365dfc32d6dcd32b1dc4da83f492b6
AI3I <ai3i@users.noreply.github.com>
2026-05-10 11:31:27 -0400

    Repair invalid strings catalog during upgrade

--- deploy/repair.sh
+++ deploy/repair.sh
@@ -19,6 +19,7 @@ ensure_selinux_contexts
 install_config_if_missing
 install_optional_config_if_missing "config/strings.toml" "strings.toml"
 install_optional_config_if_missing "config/pycluster.local.toml.example" "pycluster.local.toml.example"
+validate_or_refresh_strings_toml
 install_or_refresh_service
 ensure_fail2ban_packages
 install_or_refresh_fail2ban
